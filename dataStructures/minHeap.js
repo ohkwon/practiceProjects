4e418abbc03c935f63dea4e7fc47bcdd4c388d6b
ohoonIoMac <ohoonkwon@OHoon-Kwons-MacBook-Air.local>
2020-02-05 17:13:45 -0800

    simplify add function in minHeap.js

--- dataStructures/minHeap.js
+++ dataStructures/minHeap.js
@@ -1,56 +1,67 @@
 class MinHeap {
 	#heap;
 	#length = 0;
-
+    //************************************************************************
+    // 
+    //************************************************************************
 	constructor() {
 		this.#heap   = [];
 		this.#length = 0;
 	}
-
+    //************************************************************************
+    // 
+    //************************************************************************
 	add(value) {
 		this.#heap.push(value);
 		this.#length++;
 		var newIndex    = this.#length - 1;
 		var parentIndex = this.getParent(newIndex);
 
-		if (typeof this.#heap[parentIndex] !== "undefined") {
-			var parentValue = this.#heap[parentIndex];
-
-    		while (parentIndex >= 0 && parentValue > value) {
-    			this.#heap[newIndex] = parentValue;
-    			newIndex             = parentIndex;
-    			this.#heap[newIndex] = value;
-    			parentIndex          = this.getParent(newIndex)
-
-    			if (parentIndex >= 0 && typeof this.#heap[parentIndex] !== "undefined") {
-    				parentValue      = this.#heap[parentIndex];
-    			} else {
-    				break;
-    			}
-    		}
+        while (parentIndex >= 0 && this.#heap[parentIndex] > this.#heap[newIndex]) {
+            this.swap(newIndex, parentIndex);
+            newIndex    = parentIndex;
+			parentIndex = this.getParent(newIndex);
 		}
 	}
-
+    //************************************************************************
+    // 
+    //************************************************************************
 	getParent(index) {
 		return Math.floor((index - 1) / 2);
 	}
-
+    //************************************************************************
+    // 
+    //************************************************************************
 	getLeft(index) {
 		return index * 2 + 1;
 	}
-
+    //************************************************************************
+    // 
+    //************************************************************************
 	getRight(index) {
 		return index * 2 + 2;
 	}
-
+    //************************************************************************
+    // 
+    //************************************************************************
 	show() {
         var output = "[";
+        
 		for (var i = 0; i < this.#heap.length; i++) {
             output += i + ": " + this.#heap[i] + ", ";
         }
 
         console.log(output + "]");
 	}
+    //************************************************************************
+    // 
+    //************************************************************************
+    swap(index1, index2) {
+        var value1 = this.#heap[index1];
+        var value2 = this.#heap[index2];
+        this.#heap[index1] = value2;
+        this.#heap[index2] = value1;
+    }
 }
 
 var test = new MinHeap();
