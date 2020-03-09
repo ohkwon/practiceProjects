bc6caac317a10162c4657117990b956b74c00245
ohoon_desktop <ohoon.kw@gmail.com>
2020-03-08 23:01:05 -0700

    start 238

---
+++ problems/238_productExceptSelf.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    var output = [];
+
+    for(var i = 0; i < nums.length; i++) {
+        for (var j = 0; j < nums.length; i++) {
+            if (j != i) {
+                
+            }
+        }
+    }
+};
