40f278923818db4a3dabc7a2d80984c23670cf97
ohoon_desktop <ohoon.kw@gmail.com>
2020-03-09 23:24:15 -0700

    problem 238 created unoptimal solution, wrote out optimal time solution, need optimal time and space soltuion still

--- problems/238_productExceptSelf.js
+++ problems/238_productExceptSelf.js
@@ -3,13 +3,24 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
+    var l      = [];
+    var r      = [];
     var output = [];
 
-    for(var i = 0; i < nums.length; i++) {
-        for (var j = 0; j < nums.length; i++) {
-            if (j != i) {
-                
-            }
-        }
+    l[0]               = 1;
+    r[nums.length - 1] = 1;
+
+    for (var i = 1; i < nums.length; i++) {
+        l[i] = l[i - 1] * nums[i - 1];
     }
-};
+
+    for (var i = nums.length - 2; i >= 0; i--) {
+        r[i] = r[i + 1] * nums[i + 1];
+    }
+
+    for (var i = 0; i < nums.length; i++) {
+        output[i] = l[i] * r[i];
+    }
+
+    return output;
+}
---
+++ problems/238_productExceptSelf_unoptimal.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    var output   = [];
+    var currArr  = nums;
+    var computed = [nums];
+
+    while (currArr.length > 2) {
+        var computedI       = computed.length;
+        computed[computedI] = [];
+
+        for (var i = 0; i < currArr.length; i++) {
+            var computedSubI = Math.floor(i / 2);
+
+            if (typeof computed[computedI][computedSubI] === 'undefined') {
+                computed[computedI][computedSubI] = currArr[i];
+            } else {
+                computed[computedI][computedSubI] *= currArr[i];
+            }
+        }
+
+        currArr = computed[computedI];
+    }
+
+    for (var i = 0; i < nums.length; i++) {
+        var j = 0;
+
+        while (j < nums.length) {
+            if (j !== i) {
+                for (var computedI = computed.length - 1; computedI >= 0; computedI--) {
+                    var power     = Math.pow(2, computedI);
+                    var jQuotient = Math.floor(j / power);
+                    var iQuotient = Math.floor(i / power);
+
+                    if (jQuotient !== iQuotient && typeof computed[computedI][jQuotient] !== 'undefined') {
+                        if (typeof output[i] === 'undefined') {
+                            output[i] = computed[computedI][jQuotient];
+                        } else {
+                            output[i] *= computed[computedI][jQuotient];
+                        }
+
+                        j += power;
+                        break;
+                    }
+                }
+            } else {
+                j++;
+            }
+        }
+    }
+
+    return output;
+};
+
+const test = [1,2,3,4];
+console.log(productExceptSelf(test));
